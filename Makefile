570df2eccc543974239cb1549895192444720252
Krisztian Litkey <krisztian.litkey@intel.com>
2016-10-05 16:47:35 +0300

    repo: initial import.

---
+++ Makefile
@@ -0,0 +1,98 @@
+ARCH      = x86_64
+QEMUARCH  = qemux86-64
+VERSION   = 0.0.1
+
+TOPDIR    = $(CURDIR)
+REPO      = $(TOPDIR)/repo
+POKY      = $(TOPDIR)/../poky
+IMAGEDIR  = $(POKY)/build/tmp/deploy/images
+
+TARDIR      = $(IMAGEDIR)/$(QEMUARCH)
+TAR_XFORM   = --transform 's,^./usr,files,S' \
+              --transform 's,^./etc,files/etc,S'
+TAR_EXCLUDE = --exclude='./[!eu]*'
+
+RUNTIME_IMAGE    = flatpak-runtime-image
+RUNTIME_TAR      = $(TARDIR)/$(RUNTIME_IMAGE)-$(QEMUARCH).tar.bz2
+RUNTIME_MANIFEST = $(TARDIR)/$(RUNTIME_IMAGE)-$(QEMUARCH).manifest
+RUNTIME_BRANCH   = runtime/org.yocto.BasePlatform/$(ARCH)/$(VERSION)
+RUNTIME_METADATA = metadata.runtime
+
+SDK_IMAGE        = flatpak-sdk-image
+SDK_TAR          = $(TARDIR)/$(SDK_IMAGE)-$(QEMUARCH).tar.bz2
+SDK_MANIFEST     = $(TARDIR)/$(SDK_IMAGE)-$(QEMUARCH).manifest
+SDK_BRANCH       = runtime/org.yocto.BaseSdk/$(ARCH)/$(VERSION)
+SDK_METADATA     = metadata.sdk
+
+TMP_RUNTIME      = .runtime.tmp
+TMP_SDK          = .sdk.tmp
+
+
+all: make-repo populate-runtime populate-sdk runtime.libs
+
+clean: clean-repo
+
+make-repo:
+	if [ ! -d $(REPO) ]; then \
+	    echo "Initializing OSTree repo $(REPO)..."; \
+	    ostree --repo=$(REPO) init --mode=archive-z2; \
+	fi
+
+clean-repo:
+	rm -fr $(REPO)
+
+populate-runtime: make-repo $(RUNTIME_TAR)
+	rm -fr $(TMP_RUNTIME) && mkdir $(TMP_RUNTIME)
+	echo "Populating $(REPO) with runtime image..."
+	(cd $(TMP_RUNTIME); \
+	    tar $(TAR_XFORM) $(TAR_EXCLUDE) -xvjf $(RUNTIME_TAR) > /dev/null)
+	cp $(RUNTIME_MANIFEST) $(TMP_RUNTIME)/files/manifest.base
+	find $(TMP_RUNTIME) -type f -name \*.pyc -exec rm -f {} \;
+	find $(TMP_RUNTIME) -type f -name \*.pyo -exec rm -f {} \;
+	cat $(RUNTIME_METADATA).in | \
+	    sed 's/@ARCH@/$(ARCH)/g;s/@VERSION@/$(VERSION)/g' \
+	        > $(TMP_RUNTIME)/metadata
+	find $(TMP_RUNTIME) -type f -exec chmod u+r {} \;
+	ostree --repo=$(REPO) commit \
+	    --owner-uid=0 --owner-gid=0 --no-xattrs \
+	    --branch=$(RUNTIME_BRANCH) -s "Runtime $(VERSION)" $(TMP_RUNTIME)
+	ostree --repo=$(REPO) summary -u
+	rm -fr $(TMP_RUNTIME)
+
+$(RUNTIME_TAR):
+	pushd $(POKY) && \
+	    source ./oe-init-build-env && \
+	    bitbake $(RUNTIME_IMAGE) && \
+	popd
+
+runtime: populate-runtime
+
+runtime.libs: $(RUNTIME_TAR)
+	tar -tjf $(RUNTIME_TAR) | grep 'lib/lib.*\.so\.' > $@
+
+
+populate-sdk: make-repo $(SDK_TAR)
+	rm -fr $(TMP_SDK) && mkdir $(TMP_SDK)
+	echo "Populating $(REPO) with SDK image..."
+	(cd $(TMP_SDK); \
+	    tar $(TAR_XFORM) $(TAR_EXCLUDE) -xvjf $(SDK_TAR) > /dev/null)
+	cp $(SDK_MANIFEST) $(TMP_SDK)/files/manifest.base
+	find $(TMP_SDK) -type f -name \*.pyc -exec rm -f {} \;
+	find $(TMP_SDK) -type f -name \*.pyo -exec rm -f {} \;
+	cat $(SDK_METADATA).in | \
+	    sed 's/@ARCH@/$(ARCH)/g;s/@VERSION@/$(VERSION)/g' \
+	        > $(TMP_SDK)/metadata
+	find $(TMP_SDK) -type f -exec chmod u+r {} \;
+	ostree --repo=$(REPO) commit \
+	    --owner-uid=0 --owner-gid=0 --no-xattrs \
+	    --branch=$(SDK_BRANCH) -s "SDK $(VERSION)" $(TMP_SDK)
+	ostree --repo=$(REPO) summary -u
+	rm -fr $(TMP_SDK)
+
+$(SDK_TAR):
+	pushd $(POKY) && \
+	    source ./oe-init-build-env && \
+	    bitbake $(SDK_IMAGE) && \
+	popd
+
+sdk: populate-sdk
---
+++ flatpak-build.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+# Print help on usage.
+print_usage () {
+    echo "usage: $0 -n <name> -s <src> -c <cmd> [optional options]"
+    echo "    <name>     application id, flatpak name will be org.flatpak.<name>"
+    echo "    <src>      sub-directory containing the application source"
+    echo "    <cmd>      command used to start up the application"
+    echo "The optional options are:"
+    echo "    -S <ns>    run with a shared given namespace"
+    echo "    -F <fs>    allow access to the given filesystem, which can be"
+    echo "               'host', 'home', or a path"
+    echo "    -R <repo>  flatpak repository to export the application to"
+    echo "    <version>  use application version/branch, defaults to 0.0.1"
+    echo ""
+    echo "For example:"
+    echo "    wget http://some.where.org/zsh-5.2.tar.gz"
+    echo "    tar -xvzf zsh-5.2.tar.gz"
+    echo "    $0 --name zsh --source zsh-5.2 --command zsh \\"
+    echo "       --share network,ipc --filesystem home"
+}
+
+# Parse the command line.
+parse_command_line () {
+    while [ -n "$1" ]; do
+        case $1 in
+            --name|-n)
+                FLATPAK_APP="$2"
+                shift 2
+                ;;
+            --src|--source|-s)
+                FLATPAK_SRC="$2"
+                shift 2
+                ;;
+            --command|-c)
+                FLATPAK_CMD="$2"
+                shift 2
+                ;;
+            --version|-v)
+                FLATPAK_VER="$2"
+                shift 2
+                ;;
+            --share|-S)
+                FLATPAK_SHARE="$FLATPAK_SHARE --share=$2"
+                shift 2
+                ;;
+            --filesystem|--fs|-F)
+                FLATPAK_FS="$FLATPAK_FS --filesystem=$2"
+                shift 2
+                ;;
+            --repo|-R)
+                FLATPAK_REPO="$2"
+                shift 2
+                ;;
+            --help|-h)
+                print_usage
+                exit 0
+                ;;
+
+            *)
+                print_usage
+                exit 1
+                ;;
+            esac
+    done
+
+    if [ -z "$FLATPAK_APP" -o -z "$FLATPAK_SRC" -o -z "$FLATPAK_CMD" ]; then
+        print_usage
+        exit 1
+    fi
+
+    builddir=".$FLATPAK_APP.builddir"
+}
+
+# Initialize the flatpak build directory.
+flatpak_init () {
+    echo "* Initializing flatpak build directory..."
+
+    rm -fr $builddir && mkdir $builddir
+    flatpak build-init $builddir \
+        org.flatpak.$FLATPAK_APP \
+        org.yocto.BaseSdk org.yocto.BasePlatform \
+        $FLATPAK_VER
+}
+
+# Configure the application for flatpak build.
+flatpak_configure () {
+    echo "* Configuring flatpak build..."
+
+    cd $FLATPAK_SRC
+
+    if [ ! -f configure ]; then
+        if [ -x autogen.sh ]; then
+            ./autogen.sh
+        elif [ -x bootstrap.sh -o -x bootstrap ]; then
+            ./bootstrap.sh || ./bootstrap
+        fi
+    fi
+
+    if [ -x configure ]; then
+        flatpak build ../$builddir ./configure --prefix=/app
+    elif [ ! -f Makefile ]; then
+        echo "Don't know how to configure this package..."
+        exit 1
+    fi
+
+    cd ..
+}
+
+# Build the application for/with flatpak.
+flatpak_build () {
+    echo "* Performing flatpak build..."
+
+    cd $FLATPAK_SRC
+
+    flatpak build ../$builddir make
+
+    cd ..
+}
+
+# Install the application for/with flatpak.
+flatpak_install () {
+    echo "* Performing flatpak install..."
+
+    cd $FLATPAK_SRC
+
+    flatpak build ../$builddir make install
+
+    cd ..
+}
+
+# Resolve libraries missing from the runtime image.
+flatpak_resolve_libs () {
+    echo "* Resolving libraries for flatpak app..."
+
+    flatpak build $builddir ./resolve-libs.sh -v \
+        -P runtime.libs -R / -F $builddir/files
+}
+
+# Finalize the flatpak build of the application.
+flatpak_finish () {
+    echo "* Finalizing flatpak build..."
+    echo "* share: $FLATPAK_SHARE"
+    echo "* filesystems: $FLATPAK_FS"
+
+    flatpak build-finish $builddir \
+        $FLATPAK_SHARE $FLATPAK_FS \
+        --command=$FLATPAK_CMD
+}
+
+# Export the application to the given repository.
+flatpak_export () {
+    echo "Exporting flatpak app to repository $FLATPAK_REPO..."
+
+    flatpak build-export $FLATPAK_REPO $builddir
+}
+
+
+#########################
+# main script
+FLATPAK_REPO="repo"
+FLATPAK_VER="0.0.1"
+
+set -e
+
+parse_command_line $*
+
+flatpak_init
+flatpak_configure
+flatpak_build
+flatpak_install
+flatpak_resolve_libs
+flatpak_finish
+flatpak_export
+
+echo $?
+
+#rm -fr $builddir && mkdir $builddir
+#flatpak build-init $builddir org.flatpak.$name \
+#    org.yocto.BaseSdk org.yocto.BasePlatform $version
+#
+#cd $dir
+#flatpak build ../$builddir ./configure --prefix=/app
+#flatpak build ../$builddir make
+#flatpak build ../$builddir make install
+#cd ..
+#
+#flatpak build $builddir ./resolve-libs.sh -v -P runtime.libs \
+#                                             -R / -F $builddir/files
+#
+#flatpak build-finish $builddir \
+#    --share=$SHARED --filesystem=$FILESYSTEMS --command=$command
+#flatpak build-export repo $builddir
+#
+
